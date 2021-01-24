9b72a7a436a6fd0ab7844cad50902851000f1097
CoderWeiLee <coderweilee@gmail.com>
2021-01-24 13:08:33 +0800

    销毁界面暂时用原来的h5界面

--- digitalCurrency/wwwwwViewController.m
+++ digitalCurrency/wwwwwViewController.m
@@ -9,8 +9,10 @@
 #import "wwwwwViewController.h"
 #import <Masonry/Masonry.h>
 #import "LWDestroyHeaderView.h"
-@interface wwwwwViewController ()<UITableViewDataSource, UITableViewDelegate>
+#import <WebKit/WebKit.h>
+@interface wwwwwViewController ()<UITableViewDataSource, UITableViewDelegate, WKNavigationDelegate>
 @property (nonatomic, strong) UITableView *tableView;
+@property(nonatomic,strong)WKWebView *webView;
 @end
 
 @implementation wwwwwViewController
@@ -18,14 +20,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationItem.title = @"销毁交易";
-    self.tableView = [[UITableView alloc]init];
-    self.tableView.dataSource = self;
-    self.tableView.delegate = self;
-    [self.view addSubview:self.tableView];
-    [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.edges.equalTo(self.view);
-    }];
-    [self.tableView registerClass:[LWDestroyHeaderView class] forHeaderFooterViewReuseIdentifier:NSStringFromClass([LWDestroyHeaderView class])];
+//    self.tableView = [[UITableView alloc]init];
+//    self.tableView.dataSource = self;
+//    self.tableView.delegate = self;
+//    [self.view addSubview:self.tableView];
+//    [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.edges.equalTo(self.view);
+//    }];
+//    [self.tableView registerClass:[LWDestroyHeaderView class] forHeaderFooterViewReuseIdentifier:NSStringFromClass([LWDestroyHeaderView class])];
+    [self.view addSubview:[self webView]];
 }
 
 
@@ -49,5 +52,57 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"" forIndexPath:indexPath];
     
     return cell;
+}
+
+-(WKWebView *)webView{
+    if (!_webView) {
+        NSString*md5Str= [YLUserInfo shareUserInfo].token;
+
+        _webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, 0, kWindowW, kWindowH-NEW_NavHeight)];
+        _webView.navigationDelegate = self;
+        [_webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"https://device.abc.tm/?token=%@",md5Str]]]];
+    }
+    return _webView;
+}
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(null_unspecified WKNavigation *)navigation {
+    [EasyShowLodingView hidenLoding];
+    NSString *js=@"var script = document.createElement('script');"
+    "script.type = 'text/javascript';"
+    "script.text = \"function ResizeImages() { "
+    "var myimg,oldwidth;"
+    "var maxwidth = %f;"
+    "for(i=0;i <document.images.length;i++){"
+    "myimg = document.images[i];"
+    "if(myimg.width > maxwidth){"
+    "oldwidth = myimg.width;"
+    "myimg.width = %f;"
+    "}"
+    "}"
+    "}\";"
+    "document.getElementsByTagName('head')[0].appendChild(script);";
+    js = [NSString stringWithFormat:js,[UIScreen mainScreen].bounds.size.width,[UIScreen mainScreen].bounds.size.width-20];
+    [webView evaluateJavaScript:js completionHandler:^(id _Nullable response, NSError * _Nullable error) {
+        
+    }];
+    [webView evaluateJavaScript:@"ResizeImages();" completionHandler:^(id _Nullable response, NSError * _Nullable error) {
+        
+    }];
+    [webView evaluateJavaScript:@"document.getElementsByTagName('body')[0].style.webkitTextFillColor= '#999999'" completionHandler:^(id _Nullable response, NSError * _Nullable error) {
+        
+    }];
+}
+
+#pragma mark - WKNavigationDelegate
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(null_unspecified WKNavigation *)navigation {
+    [EasyShowLodingView showLodingText:LocalizationKey(@"aaaaaa")];
+}
+#pragma mark - WKNavigationDelegate
+- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(null_unspecified WKNavigation *)navigation withError:(NSError *)error {
+   [EasyShowLodingView hidenLoding];
+}
+ 
+- (void)webView:(WKWebView *)webView didFailNavigation:(null_unspecified WKNavigation *)navigation withError:(NSError *)error {
+    [EasyShowLodingView hidenLoding];
 }
 @end
